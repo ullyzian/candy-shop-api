11aa44dc13940896d8242ce1dfc6b8175278d306
Viktor <viktor@Viktor-Macbook.local>
2020-03-31 19:16:53 +0200

    Crete User, OrderItem and Order models. Add debbuger settings

--- core/models.py
+++ core/models.py
@@ -6,14 +6,20 @@ class User(models.Model):
     username = models.Column(models.String(60), index=True, unique=True)
     email = models.Column(models.String(120), index=True, unique=True)
     password_hash = models.Column(models.String(128))
-    order_item = models.relationship('OrderItem', )
+    order_item = models.relationship("OrderItem", backref="author", lazy="dynamic")
+    order = models.relationship("Order", backref="author", lazy="dynamic")
+
+
+    def __repr__(self):
+        return f"User {self.username}"
 
 
 class Item(models.Model):
     id = models.Column(models.Integer, primary_key=True)
     title = models.Column(models.String(50))
-    price = models.Column(models.Integer)
+    price = models.Column(models.Float)
     description = models.Column(models.Text)
+    order_item = models.relationship("OrderItem", uselist=False, back_populates="item")
 
     def __repr__(self):
         return f"<Item {self.title}>"
@@ -21,4 +27,13 @@ class Item(models.Model):
 
 class OrderItem(models.Model):
     id = models.Column(models.Integer, primary_key=True)
-    user_id = models.Column(models.Integer, models.ForeignKey('user.id'))
+    item_id = models.Column(models.Integer, models.ForeignKey("item.id"))
+    # back reference one to one relation to "Item"
+    item = models.relationship("Item", back_populates="orderitem")
+    user_id = models.Column(models.Integer, models.ForeignKey("user.id"))
+    quantity = models.Column(models.Integer)
+
+
+class Order(models.Model):
+    id = models.Column(models.Integer, primary_key=True)
+    user_id = models.Column(models.Integer, models.ForeignKey("user.id"))
